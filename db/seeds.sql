e52686d235bc34882b99c186eb6571a3f41c8eac
Ronnieve23 <ronnieve_romero@yahoo.com>
2021-09-06 16:06:27 -0400

    added ability to update employee role

--- db/seeds.sql
+++ db/seeds.sql
@@ -8,13 +8,17 @@ INSERT INTO role (title, salary, department_id)
 VALUES
     ('Chief Executive Officer', 200000, 1),
     ('Chief Technology Officer', 150000, 1),
-    ('Director, Solutions Development', 100000, 2)
-    ('Human Resources Director', 100000, 3);
+    ('Director, Solutions Development', 100000, 2),
+    ('Human Resources Director', 100000, 3),
+    ('Developer', 85000, 2),
+    ('HR Recruiter', 50000, 3);
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
 VALUES
     ('Ron', 'Romero', 1, NULL),
     ('Zachary', 'Millburn', 2, 1),
     ('Jessie', 'Kaminsky', 3, 2),
-    ('Mikael', 'Pfishnets', 4, 1);
+    ('Mikael', 'Pfishnets', 4, 1),
+    ('Chasius', 'Brackeen', 5, 3),
+    ('Groundler', 'Byron', 6, 4);
     
--- index.js
+++ index.js
@@ -1,42 +1,45 @@
 //MYSQL
-const mysql = require ('mysql2');
+const mysql = require('mysql2');
 //Inquirer
-const inquirer = require ('inquirer');
+const inquirer = require('inquirer');
 // Console Table
-const cTable = require ('console.table');
-
+const cTable = require('console.table');
 const db = require('./db/connection');
 
-function startApp () {
+
+
+function startApp() {
     inquirer.prompt({
-            type: 'list',
-            name: 'menu',
-            message: 'What Would You Like to Do?',
-            choices: ['View All Employees', 'View All Departments', 'View All Job Roles', 'Add A Department', 'Add A Job Title','Add A New Employee'],
-        }).then ( answer => {
-            switch (answer.menu){
-                case 'View All Employees':
-                    viewEmployees ();
-                    break;
-                case 'View All Departments':
-                    viewDepartments ();
-                    break;
-                 case 'View All Job Roles':
-                    viewRoles ();
-                    break;
-                case 'Add A Department':
-                    addDepartment ();
-                    break;
-                case 'Add A Job Title':
-                    addJob ();
-                    break;
-                case 'Add A New Employee':
-                    addEmployee ();
-                    break;
-            
-            }
-        })
-        
+        type: 'list',
+        name: 'menu',
+        message: 'What Would You Like to Do?',
+        choices: ['View All Employees', 'View All Departments', 'View All Job Roles', 'Add A Department', 'Add A Job Title', 'Add A New Employee', 'Update Employee Role'],
+    }).then(answer => {
+        switch (answer.menu) {
+            case 'View All Employees':
+                viewEmployees();
+                break;
+            case 'View All Departments':
+                viewDepartments();
+                break;
+            case 'View All Job Roles':
+                viewRoles();
+                break;
+            case 'Add A Department':
+                addDepartment();
+                break;
+            case 'Add A Job Role':
+                addJob();
+                break;
+            case 'Add A New Employee':
+                addEmployee();
+                break;
+            case 'Update Employee Role':
+                updateEmployeeRole();
+                break;
+        }
+    })
+
 };
 
 function viewEmployees() {
@@ -59,7 +62,7 @@ function viewEmployees() {
     });
 };
 
-function viewDepartments () {
+function viewDepartments() {
     const sql = `SELECT * FROM department`;
     db.query(sql, (err, result) => {
         if (err) throw err;
@@ -68,7 +71,7 @@ function viewDepartments () {
     });
 };
 
-function viewRoles () {
+function viewRoles() {
     const sql = `SELECT * FROM role`;
     db.query(sql, (err, result) => {
         if (err) throw err;
@@ -77,18 +80,18 @@ function viewRoles () {
     });
 };
 
-function addDepartment (){
-    inquirer.prompt ([
+function addDepartment() {
+    inquirer.prompt([
         {
             name: 'department_name',
             type: 'input',
             message: 'What Department Would You Like to Add to the Database?'
         }
-    ]).then ((answer) => {
+    ]).then((answer) => {
         const sql = `INSERT INTO department (department_name)
                      VALUES (?)`;
         const params = [answer.department_name];
-        db.query(sql,params,(err, result) => {
+        db.query(sql, params, (err, result) => {
             if (err) throw err;
             console.log('THE DEPARTMENT HAS BEEN ADDED TO THE DATABASE');
             console.table(result);
@@ -97,34 +100,34 @@ function addDepartment (){
     });
 };
 
-function addJob () {
-    inquirer.prompt ([
+function addJob() {
+    inquirer.prompt([
         {
             name: 'title',
             type: 'input',
-            message: 'What Job Title Would You Like to Add to the Database?'
+            message: 'What Job Role Would You Like to Add to the Database?'
         },
         {
             name: 'salary',
             type: 'input',
-            message: 'How Much Is the Salary For this Job Title? (do not use commas or periods)'
+            message: 'How Much Is the Salary For this Job Role? (do not use commas or periods)'
         },
         {
             name: 'department_id',
             type: 'input',
-            message: 'Please Enter the Department ID Number the New Job Title Should Belong To:'
+            message: 'Please Enter the Department ID Number the New Job Role Should Belong To:'
         },
-    ]).then (function (response){
-        db.query("INSERT INTO role (title,salary,department_id) VALUES (?,?,?)", [response.title,response.salary,response.department_id],function (err){
-        if (err) throw (err);
-        console.log ('THE NEW JOB TITLE HAS BEEN CREATED');
-        startApp();
+    ]).then(function (response) {
+        db.query("INSERT INTO role (title,salary,department_id) VALUES (?,?,?)", [response.title, response.salary, response.department_id], function (err) {
+            if (err) throw (err);
+            console.log('THE NEW JOB ROLE HAS BEEN CREATED');
+            startApp();
         });
     });
 };
 
-function addEmployee () {
-    inquirer.prompt ([
+function addEmployee() {
+    inquirer.prompt([
         {
             name: 'first_name',
             type: 'input',
@@ -139,18 +142,47 @@ function addEmployee () {
             name: 'role_id',
             type: 'number',
             message: 'Please Enter the Role ID Number the New Employee Should Belong To:'
-        },{
+        },
+        {
             name: 'manager_id',
             type: 'number',
             message: "Please Enter the ID Number of the Manager the New Employee Would Be Reporting To:"
         },
-    ]).then (function (response){
-        db.query("INSERT INTO employee (first_name,last_name,role_id,manager_id) VALUES (?,?,?,?)", [response.first_name,response.last_name,response.role_id,response.manager_id],function (err){
-        if (err) throw (err);
-        console.log ('THE NEW JOB EMPLOYEE HAS BEEN CREATED');
-        startApp();
+    ]).then(function (response) {
+        db.query("INSERT INTO employee (first_name,last_name,role_id,manager_id) VALUES (?,?,?,?)", [response.first_name, response.last_name, response.role_id, response.manager_id], function (err) {
+            if (err) throw (err);
+            console.log('THE NEW JOB EMPLOYEE HAS BEEN CREATED');
+            startApp();
         });
     });
 };
+
+function updateEmployeeRole() {
+    inquirer.prompt([
+        {
+            name: 'first_name',
+            type: 'input',
+            message: 'Please Enter the First Name of the Employee You Want To Update'
+        },
+        {
+            name: 'last_name',
+            type: 'input',
+            message: 'Please Enter the Last Name of the Employee You Want To Update'
+        },
+        {
+            name: 'role_id',
+            type: 'number',
+            message: "Please Enter the Role ID Number For the Employee's New Role:"
+
+        }
+    ]).then(function (response) {
+        db.query("UPDATE employee SET role_id = ? WHERE first_name = ? AND last_name = ?", [response.role_id, response.first_name, response.last_name], function (err) {
+            if (err) throw err;
+            console.log("YOUR EMPLOYEE'S ROLE HAS BEEN UPDATED");
+            startApp();
+            });
+        })
+};
+
 
 startApp();
